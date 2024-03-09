209636a02d89bbc77315fdf593c237d31eecdf48
revus05 <skninni05@gmail.com>
2024-03-09 22:18:41 +0300

    friends refactor

--- src/friends/friends.controllers.ts
+++ src/friends/friends.controllers.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Req } from '@nestjs/common'
+import { Body, Controller, Get, Post, Req } from '@nestjs/common'
 import { Request } from 'express'
 import { FriendsService, GetFriendsResponse, RemoveFriendResponse } from './friends.service'
+import { RegisterCredentials } from '../types/register'
 
 @Controller('/friends')
 export class FriendsControllers {
@@ -12,7 +13,7 @@ export class FriendsControllers {
 	}
 
 	@Post('/remove')
-	async removeFriend(@Req() req: Request): Promise<RemoveFriendResponse> {
-		return await this.friendsService.removeFriend(req.cookies.jwt, req.body)
+	async removeFriend(@Req() req: Request, @Body() requestBody: { friendId: number }): Promise<RemoveFriendResponse> {
+		return await this.friendsService.removeFriend(req.cookies.jwt, requestBody)
 	}
 }
